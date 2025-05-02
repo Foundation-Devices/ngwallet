d7c9951ce2afade94d7ebd48a2ccdc2a0d616ccf
Igor Cota <igor@openbook.hr>
2025-05-02 20:20:41 +0200

    bip39: add func to generate seed and convert to descriptors

--- src/bip39.rs
+++ src/bip39.rs
@@ -1,8 +1,93 @@
-use bdk_wallet::bip39::Language;
+use bdk_wallet::KeychainKind;
+use bdk_wallet::bitcoin::Network;
+use bdk_wallet::bitcoin::bip32::Xpriv;
+use bdk_wallet::keys::bip39::{Language, Mnemonic};
+use bdk_wallet::template::{Bip84, Bip86, DescriptorTemplate};
 use std::cmp::min;
+
+pub enum DescriptorType {
+    Bip84,
+    Bip86,
+}
+
+pub struct Descriptors {
+    descriptor: String,
+    change_descriptor: String,
+    descriptor_xpub: String,
+    change_descriptor_xpub: String,
+}
 
 pub fn get_seedword_suggestions(input: &str, nr_of_suggestions: usize) -> Vec<&str> {
     let list = Language::English.words_by_prefix(input);
     let count = min(nr_of_suggestions, list.len());
     list[..count].to_vec()
+}
+
+pub fn get_random_seed() -> anyhow::Result<String> {
+    let mnemonic = Mnemonic::generate_in(Language::English, 12)?;
+    Ok(mnemonic.to_string())
+}
+
+pub fn get_descriptors(
+    seed: String,
+    descriptor_type: DescriptorType,
+    network: Network,
+    passphrase: Option<String>,
+) -> anyhow::Result<Descriptors> {
+    let mnemonic = Mnemonic::parse(seed)?;
+    let seed = mnemonic.to_seed(passphrase.unwrap_or("".to_owned()));
+
+    let xprv: Xpriv = Xpriv::new_master(network, &seed)?;
+
+    let (descriptor, key_map, change_descriptor, change_key_map) = {
+        match descriptor_type {
+            DescriptorType::Bip84 => {
+                let external = Bip84(xprv, KeychainKind::External).build(network)?;
+                let internal = Bip84(xprv, KeychainKind::Internal).build(network)?;
+
+                (external.0, external.1, internal.0, internal.1)
+            }
+            DescriptorType::Bip86 => {
+                let external = Bip86(xprv, KeychainKind::External).build(network)?;
+                let internal = Bip86(xprv, KeychainKind::Internal).build(network)?;
+
+                (external.0, external.1, internal.0, internal.1)
+            }
+        }
+    };
+
+    Ok(Descriptors {
+        descriptor: descriptor.to_string_with_secret(&key_map),
+        change_descriptor: change_descriptor.to_string_with_secret(&change_key_map),
+        descriptor_xpub: descriptor.to_string(),
+        change_descriptor_xpub: change_descriptor.to_string(),
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use crate::bip39::DescriptorType::Bip84;
+    use crate::bip39::{get_descriptors, get_random_seed};
+    use bdk_wallet::bitcoin::Network;
+
+    #[test]
+    fn test_get_descriptor_from_seed() {
+        let mnemonic =
+            "aim bunker wash balance finish force paper analyst cabin spoon stable organ"
+                .to_owned();
+
+        assert_eq!(get_descriptors(mnemonic, Bip84, Network::Bitcoin, None).unwrap().descriptor, "wpkh(xprv9s21ZrQH143K2v9ABLJujuoqaJoMuazgoH6Yg4CceWQr86hPGbE5g6ivqRnPPGTnt6GqZVTFecYEUzkB9rzj79jGenWLW9GVsG5i6CKmMAE/84'/0'/0'/0/*)#5aaucexa".to_owned());
+    }
+
+    #[test]
+    fn test_get_random_seed() {
+        assert_eq!(
+            get_random_seed()
+                .unwrap()
+                .split(' ')
+                .collect::<Vec<_>>()
+                .len(),
+            12
+        );
+    }
 }
