075245c032857f4ae003f9062382ebbd8e49e287
invertedx <invertedx@foundation.xyz>
2025-05-08 01:56:51 +0200

    test util for spend tests

--- tests/ng_test_utils.rs
+++ tests/ng_test_utils.rs
@@ -1,149 +1,104 @@
 use std::str::FromStr;
-use std::sync::{Arc, Mutex, MutexGuard};
-use bdk_wallet::{PersistedWallet, Update};
-use bdk_wallet::bitcoin::{Address, Network,Transaction,Txid,TxOut,TxIn,OutPoint,Amount,BlockHash};
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use bdk_wallet::bitcoin::hashes::Hash;
-use bdk_wallet::chain::{BlockId, ConfirmationBlockTime, tx_graph, TxUpdate};
-use bdk_wallet::rusqlite::Connection;
-use bdk_wallet::test_utils::new_tx;
+use bdk_wallet::bitcoin::{Address, Amount, BlockHash, FeeRate, Network, Transaction, TxOut};
+use bdk_wallet::chain::{BlockId, ConfirmationBlockTime};
+use bdk_wallet::test_utils::{insert_seen_at, new_tx};
+use bdk_wallet::{KeychainKind, WalletPersister};
+
 use ngwallet::account::NgAccount;
+use ngwallet::ngwallet::NgWallet;
 
-
-pub fn get_account_with_confirmed_unconfirmed(mut account:  &mut NgAccount<Connection>) {
-    let receive_address = {
-        account.next_address().unwrap()
-    };
-    let sendto_address = Address::from_str("tb1qr6lmcexy6285f3a04j2a75d39qvcdek739g8cz")
-        .expect("address")
-        .require_network(Network::Signet)
-        .unwrap();
-
-    {
-        for (index,ngwallet) in account.wallets.iter().enumerate() {
-            let mut wallet = ngwallet.bdk_wallet.lock().unwrap();
-            let tx0 = Transaction {
-                output: vec![TxOut {
-                    value: Amount::from_sat(76_000),
-                    script_pubkey: receive_address[index].script_pubkey(),
-                }],
-                ..new_tx(0)
-            };
-
-            let tx0 = Transaction {
-                output: vec![TxOut {
-                    value: Amount::from_sat(76_000),
-                    script_pubkey: receive_address[index].script_pubkey(),
-                }],
-                ..new_tx(0)
-            };
-
-            let tx1 = Transaction {
-                input: vec![TxIn {
-                    previous_output: OutPoint {
-                        txid: tx0.compute_txid(),
-                        vout: 0,
-                    },
-                    ..Default::default()
-                }],
-                output: vec![
-                    TxOut {
-                        value: Amount::from_sat(50_000),
-                        script_pubkey: receive_address[index].script_pubkey(),
-                    },
-                    TxOut {
-                        value: Amount::from_sat(25_000),
-                        script_pubkey: sendto_address.script_pubkey(),
-                    },
-                ],
-                ..new_tx(0)
-            };
-
-            insert_checkpoint(
-                &mut wallet,
-                BlockId {
-                    height: 42,
-                    hash: BlockHash::all_zeros(),
-                },
-            );
-            insert_checkpoint(
-                &mut wallet,
-                BlockId {
-                    height: 1_000,
-                    hash: BlockHash::all_zeros(),
-                },
-            );
-            insert_checkpoint(
-                &mut wallet,
-                BlockId {
-                    height: 2_000,
-                    hash: BlockHash::all_zeros(),
-                },
-            );
-            //
-            insert_tx(&mut wallet,tx0.clone());
-            insert_anchor(
-                &mut wallet,
-                tx0.compute_txid(),
-                ConfirmationBlockTime {
-                    block_id: BlockId {
-                        height: 1_000,
-                        hash: BlockHash::all_zeros(),
-                    },
-                    confirmation_time: 100,
-                },
-            );
-            //
-            insert_tx(&mut wallet, tx1.clone());
-            insert_anchor(
-                &mut wallet,
-                tx1.compute_txid(),
-                ConfirmationBlockTime {
-                    block_id: BlockId {
-                        height: 0,
-                        hash: BlockHash::all_zeros(),
-                    },
-                    confirmation_time: 0,
-                },
-            );
-        }
-
+pub fn add_funds_to_wallet<P: WalletPersister>(account: &mut NgAccount<P>) {
+    for (index, ngwallet) in account.wallets.iter().enumerate() {
+        fill_with_txes(index, &ngwallet)
     }
-
-    // let update = NgWallet::<Connection>::scan(request, ELECTRUM_SERVER, None).unwrap();
-    // account.wallet.apply(Update::from(update)).unwrap();
 }
 
-pub fn insert_tx(wallet: &mut MutexGuard<PersistedWallet<Connection>>, tx: Transaction) {
+pub fn add_funds_wallet_with_unconfirmed<P: WalletPersister>(account: &mut NgAccount<P>) {
+    for (index, ngwallet) in account.wallets.iter().enumerate() {
+        fill_with_txes(index, &ngwallet);
+    }
+    fill_with_unconfirmed(&account.get_coordinator_wallet());
+}
+
+fn fill_with_unconfirmed<P: WalletPersister>(ngwallet: &&NgWallet<P>) {
+    let mut wallet = ngwallet.bdk_wallet.lock().unwrap();
+    let to_address =
+        Address::from_str("tb1pspfcrvz538vvj9f9gfkd85nu5ty98zw9y5e302kha6zurv6vg07s8z7a8w")
+            .unwrap()
+            .require_network(Network::Signet)
+            .unwrap();
+    let mut psbt = {
+        let mut builder = wallet.build_tx();
+        builder
+            .add_recipient(to_address.script_pubkey(), Amount::from_sat(800))
+            .fee_rate(FeeRate::from_sat_per_vb(1).unwrap());
+        builder.finish().unwrap()
+    };
+
     wallet
-        .apply_update(Update {
-            tx_update: TxUpdate {
-                txs: vec![Arc::new(tx)],
-                ..Default::default()
+        .sign(&mut psbt, Default::default())
+        .expect("Failed to sign PSBT");
+
+    let transaction = psbt.extract_tx().unwrap();
+    // Insert the transaction into the wallet
+    let txid = transaction.compute_txid();
+    bdk_wallet::test_utils::insert_tx(&mut wallet, transaction.clone());
+
+    // Mark the transaction as unconfirmed in the mempool
+    let last_seen = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("")
+        .as_secs();
+    insert_seen_at(&mut wallet, txid, last_seen)
+}
+
+fn fill_with_txes<P: WalletPersister>(index: usize, ngwallet: &&NgWallet<P>) {
+    let amounts = [76_000, 25_000];
+    let mut wallet = ngwallet.bdk_wallet.lock().unwrap();
+    let receive_address = wallet.next_unused_address(KeychainKind::External).address;
+
+    let tx0 = Transaction {
+        output: vec![TxOut {
+            value: Amount::from_sat(amounts[index]),
+            script_pubkey: receive_address.script_pubkey(),
+        }],
+        ..new_tx(0)
+    };
+    bdk_wallet::test_utils::insert_checkpoint(
+        &mut wallet,
+        BlockId {
+            height: 42,
+            hash: BlockHash::all_zeros(),
+        },
+    );
+    bdk_wallet::test_utils::insert_checkpoint(
+        &mut wallet,
+        BlockId {
+            height: 1_000,
+            hash: BlockHash::all_zeros(),
+        },
+    );
+    bdk_wallet::test_utils::insert_checkpoint(
+        &mut wallet,
+        BlockId {
+            height: 2_000,
+            hash: BlockHash::all_zeros(),
+        },
+    );
+
+    bdk_wallet::test_utils::insert_tx(&mut wallet, tx0.clone());
+    bdk_wallet::test_utils::insert_anchor(
+        &mut wallet,
+        tx0.compute_txid(),
+        ConfirmationBlockTime {
+            block_id: BlockId {
+                height: 1_000,
+                hash: BlockHash::all_zeros(),
             },
-            ..Default::default()
-        })
-        .unwrap();
-}
-
-pub fn insert_anchor(wallet: &mut MutexGuard<PersistedWallet<Connection>>, txid: Txid, anchor: ConfirmationBlockTime) {
-    wallet
-        .apply_update(Update {
-            tx_update: tx_graph::TxUpdate {
-                anchors: [(anchor, txid)].into(),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .unwrap();
-}
-
-pub fn insert_checkpoint(wallet: &mut MutexGuard<PersistedWallet<Connection>>, block: BlockId) {
-    let mut cp = wallet.latest_checkpoint();
-    cp = cp.insert(block);
-    wallet
-        .apply_update(Update {
-            chain: Some(cp),
-            ..Default::default()
-        })
-        .unwrap();
+            confirmation_time: 100,
+        },
+    );
 }
