0ad0bff986489676f33b8ebf9461ee74c0ac1571
invertedx <invertedx@foundation.xyz>
2025-08-03 22:14:36 +0200

    fix typo

--- src/config.rs
+++ src/config.rs
@@ -55,10 +55,10 @@ pub struct NgAccountConfig {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct NgAccountBackup {
     pub ng_account_config: NgAccountConfig,
-    //envoy 2.1 doesnt include xfp in backup
+    //envoy 2.0.1 doesnt include xfp in backup
     #[serde(default)]
     pub xfp: String,
-    //envoy 2.1 doesnt include public_descriptors in backup
+    //envoy 2.0.1 doesnt include public_descriptors in backup
     #[serde(default)]
     pub public_descriptors: Vec<(AddressType, String)>,
     pub last_used_index: Vec<(AddressType, KeychainKind, u32)>,
