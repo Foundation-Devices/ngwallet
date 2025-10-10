e4fc7a1db3d6219802d3282ad896963257dcd534
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2025-10-10 17:59:26 +0200

    SFT-5973: Support P2WPKH-nested-in-P2SH.

--- src/psbt/p2sh.rs
+++ src/psbt/p2sh.rs
@@ -1,7 +1,14 @@
-use crate::psbt::{Error, OutputKind, PsbtOutput};
-use bdk_wallet::bitcoin::bip32::ChildNumber;
+use crate::bip32::NgAccountPath;
+use crate::psbt::{
+    Error, OutputKind, PsbtOutput, derive_account_xpub, derive_full_descriptor_pubkey,
+};
+use bdk_wallet::bitcoin::bip32::{ChildNumber, Xpriv};
 use bdk_wallet::bitcoin::psbt;
+use bdk_wallet::bitcoin::secp256k1::{Secp256k1, Signing};
 use bdk_wallet::bitcoin::{Address, CompressedPublicKey, Network, TxOut};
+use bdk_wallet::descriptor::ExtendedDescriptor;
+use bdk_wallet::miniscript::descriptor::Wpkh;
+use bdk_wallet::template::{Bip49Public, DescriptorTemplate};
 
 pub fn validate_output(
     output: &psbt::Output,
@@ -59,4 +66,42 @@ fn validate_p2wpkh_nested_in_p2sh_output(
         amount: txout.value,
         kind: OutputKind::from_derivation_path(&source.1, 49, network, address)?,
     })
+}
+
+/// Compute the account descriptor for P2WPKH from the `path` derivation path.
+pub fn p2shwpkh_descriptor<C>(
+    secp: &Secp256k1<C>,
+    master_key: &Xpriv,
+    path: impl AsRef<[ChildNumber]>,
+    network: Network,
+) -> ExtendedDescriptor
+where
+    C: Signing,
+{
+    match NgAccountPath::parse(&path) {
+        Ok(Some(account_path)) => {
+            // Not a valid BIP-0049 derivation path, just return the full derivation path and
+            // the computed public key.
+            if !account_path.matches(49, network) || !account_path.is_for_address() {
+                let pk = derive_full_descriptor_pubkey(secp, master_key, path);
+                return ExtendedDescriptor::new_sh_with_wpkh(Wpkh::new(pk).unwrap());
+            }
+
+            let xpub = derive_account_xpub(secp, master_key, path);
+            Bip49Public(
+                xpub,
+                master_key.fingerprint(secp),
+                account_path
+                    .keychain_kind()
+                    .expect("is_for_address checks for this"),
+            )
+            .build(network)
+            .unwrap()
+            .0
+        }
+        _ => {
+            let pk = derive_full_descriptor_pubkey(secp, master_key, path);
+            ExtendedDescriptor::new_sh_with_wpkh(Wpkh::new(pk).unwrap())
+        }
+    }
 }
